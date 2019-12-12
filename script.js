f74cadae08fe34e74a760d76cc3700daaff27fba
Stephanie A <stephanie.m.aurelio@gmail.com>
2019-12-12 08:28:59 -0800

    added to portfolio gallery

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function animateImgThree() {
     var el3 = document.getElementById('portfolio-project');
     el3.setAttribute('src', 'assets/images/portfolio-icon-3b.png');
 }
+function animateImgFour() {
+    var el3 = document.getElementById('template-engine');
+    el3.setAttribute('src', 'assets/images/portfolio-icon-4b.png');
+}
+function animateImgFive() {
+    var el3 = document.getElementById('developer-profile');
+    el3.setAttribute('src', 'assets/images/portfolio-icon-5b.png');
+}
+function animateImgSix() {
+    var el3 = document.getElementById('note-taker');
+    el3.setAttribute('src', 'assets/images/portfolio-icon-6b.png');
+}
 
 document.querySelector('#portfolio-weather').addEventListener('mouseover', function() {
     animateImgOne();
@@ -34,5 +46,29 @@ document.querySelector('#portfolio-project').addEventListener('mouseover', funct
     setTimeout(function(){
         var el = document.getElementById('portfolio-project');
         el.setAttribute('src', 'assets/images/portfolio-icon-3.png');
+    }, 700);
+},false); 
+
+document.querySelector('#template-engine').addEventListener('mouseover', function() {
+    animateImgFour();
+    setTimeout(function(){
+        var el = document.getElementById('template-engine');
+        el.setAttribute('src', 'assets/images/portfolio-icon-4.png');
+    }, 700);
+},false); 
+
+document.querySelector('#developer-profile').addEventListener('mouseover', function() {
+    animateImgFive();
+    setTimeout(function(){
+        var el = document.getElementById('developer-profile');
+        el.setAttribute('src', 'assets/images/portfolio-icon-5.png');
+    }, 700);
+},false); 
+
+document.querySelector('#note-taker').addEventListener('mouseover', function() {
+    animateImgSix();
+    setTimeout(function(){
+        var el = document.getElementById('note-taker');
+        el.setAttribute('src', 'assets/images/portfolio-icon-6.png');
     }, 700);
 },false); 
